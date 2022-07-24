5011bc51153898b65da7b7432c37f09b8be88920
Amy Gardiner <amymgardiner@gmail.com>
2022-07-24 13:33:27 -0500

    import and use the fs library to write data

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
-const PORT = process.env.PORT || 3001;
+// instantiate the server
 const app = express();
 
+// tell app to use heroku's environment variable port, if it has been set, and if not, default to port 3001
+const PORT = process.env.PORT || 3001;
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+app.use(express.static('public'));
+
+
+// chain the listen() method onto our server
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
