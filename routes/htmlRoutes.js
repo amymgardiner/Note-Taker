1bb250e224e3683a163d1cf706a371ba9683d325
Amy Gardiner <amymgardiner@gmail.com>
2022-07-24 14:12:08 -0500

    routes weren't working so tried app.get in server.js

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,12 @@
+// const express = require("express");
+// const router = express.Router();
+// const path = require("path");
+
+// router.get('/notes', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/notes.html'));
+// });
+
+// // The * route should always come last. Otherwise, it will take precedence over named routes
+// router.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/index.html'));
+// });
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const express = require('express');
 
 // instantiate the server
 const app = express();
@@ -8,15 +8,31 @@ const app = express();
 // tell app to use heroku's environment variable port, if it has been set, and if not, default to port 3001
 const PORT = process.env.PORT || 3001;
 
-const apiRoutes = require('./routes')
-
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// middleware that instructs the server to make certain files readily available and to not gate it behind a server endpoint
 app.use(express.static('public'));
 
-app.use(apiRoutes);
+let { notes } = require("./db/db.json");
+
+app.get("/api/notes", (req, res) => {
+    res.json(notes);
+});
+  
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+  
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 
 // chain the listen() method onto our server
 app.listen(PORT, () => {
